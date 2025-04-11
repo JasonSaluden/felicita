de227504efc91aca448e4f53ab69e25812cc6790
JasonSaluden <saluden.jason@gmail.com>
2025-04-11 15:18:58 +0200

    Init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Navbar } from "./components/navbar";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <Navbar/>
+      <Navbar/>
+      <h1 className="text-4xl font-bold text-green-600">Hello World , GEVES🚀</h1>
+    </div>
+  )
+}
+
+export default App;
