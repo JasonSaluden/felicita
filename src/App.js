f786da753a780936cfc803a1dc02de2c8478c7ff
JasonSaluden <saluden.jason@gmail.com>
2025-04-14 20:21:53 +0200

    // Site en construction

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@ import { Navbar } from "./components/navbar";
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center h-screen text-center space-y-6">
+            <img src="/assets/logo_fest.png" alt="Logo Felicita" className="h-40 w-40 mb-4" />
+      <h1 className="text-4xl font-bold text-grey-600">
+        Bienvenue sur le site de la Felicita Festival !
+      </h1>
       
-      <h1 className="text-4xl font-bold text-green-600">Bienvenue sur le site officiel de la Felicita</h1>
+      <p className="text-xl text-gray-700">🚧 Nous sommes actuellement en construction 🚧</p>
+      <p className="text-xl text-gray-700">👇 En attendant retrouvez-nous ici 👇</p>
+
+      <div className="flex space-x-6">
+      <a href="https://www.instagram.com/lafelicitafestival/" target="_blank" rel="noopener noreferrer">
+          <img src="/assets/icons8-instagram-96.png" alt="Instagram" className="w-20 h-20" />
+        </a>
+        <a href="https://linktr.ee/felicitafestival" target="_blank" rel="noopener noreferrer">
+          <img src="/assets/linktree@logotyp.us.svg" alt="Linktree" className="w-40 h-20" />
+        </a>
+        <a href="https://www.helloasso.com/associations/la-felicita-festival" target="_blank" rel="noopener noreferrer">
+          <img src="/assets/helloAssoLogo.avif" alt="HelloAsso" className="w-20 h-20" />
+        </a>
+        <a href="https://www.facebook.com/lafelicitafestival" target="_blank" rel="noopener noreferrer">
+          <img src="/assets/facebook@logotyp.us.svg" alt="Facebook" className="w-20 h-20" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
