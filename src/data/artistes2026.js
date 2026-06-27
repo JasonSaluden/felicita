a2ab59228a01dcc45cf850b0057c0c45f6189c17
JasonSaluden <saluden.jason@gmail.com>
2026-06-27 17:30:16 +0200

    ajout infos artistes

--- src/data/artistes2026.js
+++ src/data/artistes2026.js
@@ -40,10 +40,10 @@ export const artistes2026 = [
    {
    id: 1,                                   
    name: "La Clara Sofia",
-   image: "/assets/artistes/2026/.webp",
-   image2: "/assets/artistes/2026/.webp",
+   image: "/assets/artistes/2026/LaClaraSofia_Slide1.jpg",
+   image2: "/assets/artistes/2026/LaClaraSofia_Slide1.jpg",
    description:
-     "",
+     "“Je suis immense, je contiens des multitudes”, des mots que La Clara Sofia emprunte à Walt Whitman et qui irriguent sa musique écrite en portugais et en anglais, se faisant l’écho des villes et des vies qu’elle a vécues. Sur scène, avec pour leitmotiv de faire bouger les corps et les âmes, dans un trio voix-basse-objets percussifs, le public est enveloppé dans un cocon de pop alternative tissé aux fils du Brésil.",
    genre: "Techno",
    time: "18h",                                
    socialLinks: {                           
@@ -59,8 +59,8 @@ export const artistes2026 = [
   {
     id: 2,                                   
     name: "Kriill",
-    image: "/assets/artistes/2026/.webp",
-    image2: "/assets/artistes/2026/.webp",
+    image: "/assets/artistes/2026/Kriill_Slide1.jpg",
+    image2: "/assets/artistes/2026/Kriill_Slide1.jpg",
     description:
       "",
     genre: "Techno",
@@ -78,14 +78,14 @@ export const artistes2026 = [
   {
     id: 3,                                   
     name: "No Sex Last Night",
-    image: "/assets/artistes/2026/.webp",
-    image2: "/assets/artistes/2026/.webp",
+    image: "/assets/artistes/2026/NoSexLastNight_Slide1.jpg",
+    image2: "/assets/artistes/2026/NoSexLastNight_Slide1.jpg",
     description:
-      "",
+      "Le son new wave de No Sex Last Night sent la sueur, l’angoisse et les nuits sans fin. Le duo transforme ses névroses de zoomers en beats sombres, bruts et profondément vivants. Avec des textes en français, à la fois naïfs et cruellement lucides pour exister un peu plus fort.",
     genre: "Techno",
     time: "18h",                                
     socialLinks: {                           
-      instagram: "https://instagram.com/...",
+      instagram: "https://www.instagram.com/nosexlastnightt/?hl=fr",
       soundcloud: "https://soundcloud.com/...",
       site: "https://...",
     },
@@ -97,8 +97,8 @@ export const artistes2026 = [
   {
     id: 4,                                   
     name: "NAO",
-    image: "/assets/artistes/2026/.webp",
-    image2: "/assets/artistes/2026/.webp",
+    image: "/assets/artistes/2026/Nao_Slide1.jpeg",
+    image2: "/assets/artistes/2026/Nao_Slide1.jpeg",
     description:
       "",
     genre: "Techno",
@@ -119,7 +119,7 @@ export const artistes2026 = [
     image: "/assets/artistes/2026/.webp",
     image2: "/assets/artistes/2026/.webp",
     description:
-      "",
+      "Simon Lahure aka Dj Startup est à la recherche la formule occulte qui permettra relancer l’épidémie de danse de Strasbourg en 1518. Dans un flow exaltant de mélodies midi, guimbardes, flutiots et ocarinas souffleront la musique d’une teuf antédiluvienne.",
     genre: "Techno",
     time: "18h",                                
     socialLinks: {                           
